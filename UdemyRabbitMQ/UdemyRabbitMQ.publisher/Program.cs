cdb03855c1550ff8b855c092089fabab984f8f86
Erhan Basturk <56194980+basturkerhan@users.noreply.github.com>
2022-09-26 14:47:51 +0300

    Initial commit

---
+++ UdemyRabbitMQ/UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.publisher
+{
+    public enum LogNames
+    {
+        Critical=1,
+        Error=2,
+        Warning=3,
+        Info=4,
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = Environment.GetEnvironmentVariable("URI", EnvironmentVariableTarget.Process);
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("headers-exchange", durable:true, type:ExchangeType.Headers);
+            
+            Dictionary<string,object> headers = new Dictionary<string, object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = headers;
+
+            channel.BasicPublish("headers-exchange",
+                                 String.Empty, 
+                                 properties,
+                                 Encoding.UTF8.GetBytes("header mesajı"));
+            
+            Console.WriteLine("Mesaj Gönderilmiştir");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQ/UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.subscriber // consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = Environment.GetEnvironmentVariable("URI", EnvironmentVariableTarget.Process);
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("headers-exchange", durable: true, type: ExchangeType.Headers);
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            // Kuyruk oluşturma
+            var queueName = channel.QueueDeclare().QueueName;
+            
+            Dictionary<string,object> headers = new Dictionary<string, object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            headers.Add("x-match", "all");
+
+            channel.QueueBind(queueName, "headers-exchange", String.Empty, headers);
+            channel.BasicConsume(queueName, false, consumer);
+            Console.WriteLine("Loglar Dinleniyor...");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(500);
+                Console.WriteLine("Kuyruktan Gelen Mesaj: " + message);
+
+                channel.BasicAck(e.DeliveryTag, false); // mesajı artık kuyruktan silebilirsin diye belirttik (yukarıyı false yaptığımız için)
+            };
+
+            Console.ReadLine();
+        }
+
+    }
+}
